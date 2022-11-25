e913492436e253a8d3fafdd3dc4e1038108bd653
LuisEduardo-M <luiseduardomc019@gmail.com>
2022-11-25 18:07:08 -0300

    enabling cors on routes

---
+++ go-backend/cmd/api/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	// Create a router mux (multiplexer)
+	mux := chi.NewRouter()
+
+	// Middleware
+	mux.Use(middleware.Recoverer)
+
+	// CORS
+	mux.Use(app.enableCORS)
+
+	// Routes
+	mux.Get("/", app.Home)
+	mux.Get("/movies", app.AllMovies)
+	return mux
+}
