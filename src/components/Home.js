d57a4aefada9d1d44fac2a3c6128581eec35d249
LuisEduardo-M <luiseduardomc019@gmail.com>
2022-11-25 18:01:50 -0300

    home page component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div className='text-center'>
+                <h2>Pick a Movie !</h2>
+                <hr />
+            </div>
+        </>
+    )
+}
+
+export default Home;
