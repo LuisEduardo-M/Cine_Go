19173263b446e0d71e479786973d0179f9a59eb9
LuisEduardo-M <luiseduardomc019@gmail.com>
2022-12-07 00:29:34 -0300

    Displaying movie from backend

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,23 +7,44 @@ const Movie = () => {
     let { id } = useParams();
 
     useEffect(() => {
-        let myMovie = {
-            id: 1,
-            title: "Scarface",
-            release_date: "17-04-1984",
-            runtime: 170,
-            mpaa_rating: "R",
-            description: "Some descript for Scarface"
+        const headers = new Headers();
+        headers.append("Content-Type", "applicatio/json");
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
         }
 
-        setMovie(myMovie);
+        fetch(`/movie/${id}`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                setMovie(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }, [id])
+
+    if (movie.genres) {
+        movie.genres = Object.values(movie.genres);
+    } else {
+        movie.genres = [];
+    }
 
     return (
         <div>
             <h2>Movie: {movie.title}</h2>
-            <small>{movie.release_date}, {movie.runtime} minutes - Rated {movie.mpaa_rating}</small>
+            <small>{movie.release_date}, {movie.runtime} minutes - Rated {movie.mpaa_rating}</small> <br />
+            {movie.genres.map(g => (
+                <span key={g.genre} className="badge bg-secondary me-2">{g.genre}</span>
+            ))}
             <hr />
+
+            {movie.image !== "" && 
+                <div className="mb-3">
+                    <img src={`http://image.tmdb.org/t/p/w200/${movie.image}`} alt="movie_poster" />
+                </div>
+            }
 
             <p>{movie.description}</p>
         </div>
