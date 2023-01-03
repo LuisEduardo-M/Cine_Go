5e097824275d78c7f6fb1b9d581bfe9003584fa4
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-01-03 01:02:11 -0300

    prep for deploy

--- go-backend/cmd/api/middleware.go
+++ go-backend/cmd/api/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://cine-go.vercel.app")
+		w.Header().Set("Access-Control-Allow-Origin", "https://cinegolang.herokuapp.com/")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
